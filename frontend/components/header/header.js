c175db3538674b8d4b4c6d807eac72062dbf1c64
leticix4 <leticixcardos0@gmail.com>
2025-11-28 02:51:59 -0300

    [#3] [FrontEnd] feat: integração do login

--- frontend/components/header/header.js
+++ frontend/components/header/header.js
@@ -15,6 +15,8 @@ class AppHeader extends HTMLElement {
 
   connectedCallback() {
     this.themeConfig();
+    this.checkUser();
+    this.addHeaderListeners();
   }
 
   themeConfig() {
@@ -37,6 +39,74 @@ class AppHeader extends HTMLElement {
     
     const saved = localStorage.getItem("theme");
     if (saved) document.documentElement.setAttribute("data-bs-theme", saved);
+  }
+
+  checkUser() {
+    const token = localStorage.getItem("access_token");
+    if(!token) return;
+
+    const url = "http://localhost:8000/api/user";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    }).then(res =>  res.json())
+      .then(data => {
+        const user = data.user
+        const userName = this.querySelector("#headerUserName");
+        userName.innerHTML = user.name;
+
+        this.querySelector("#btnEntrar").style.display = "none";
+        this.querySelector("#btnPerfil").style.display = "flex";
+      })
+      .catch(err => {
+        console.error("Erro ao buscar dados do usuário:", err);
+        localStorage.removeItem("access_token");
+        location.reload();
+      });
+  }
+
+  addHeaderListeners() {
+    this.addLoginListener();
+    this.addLogoutListener();
+  }
+
+  addLoginListener() {
+    const loginButton = this.querySelector("#loginButton");
+    loginButton.addEventListener("click", () => {
+      const emailInput = this.querySelector("#loginEmail");
+      const passwordInput = this.querySelector("#loginPassword");
+
+      const body = {
+        email: emailInput.value,
+        password: passwordInput.value
+      }
+
+      const backURL = "http://localhost:8000/api/login";
+
+      fetch(backURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      }).then(res => res.json())
+        .then(data => {
+          if (!data.access_token) return;
+
+          localStorage.setItem("access_token", data.access_token);
+          location.reload();
+        });
+    })
+  }
+
+  addLogoutListener() {
+    const profileButton = this.querySelector("#btnPerfil");
+    profileButton.addEventListener("click", () => {
+      localStorage.removeItem("access_token");
+      location.reload();
+    });
   }
 }
 
